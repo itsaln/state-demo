40dee6d8d4d3c7add005a1492f4e3862d04ba47b
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-05-17 11:57:17 +0500

    Хранение состояния в URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,39 @@
 'use client'
 
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 import { Catalog } from '@/components/catalog/Catalog'
 import { Filter } from '@/components/filter/Filter'
 import { TypeEnum, DirectionEnum } from '@/components/filter/constants'
 
 export default function Home() {
+	const router = useRouter()
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
+
 	const [courseType, setCourseType] = useState(TypeEnum.COURSE)
 	const [courseDirection, setCourseDirection] = useState(DirectionEnum.FRONTEND)
 
 	const setType = (value: TypeEnum) => {
 		setCourseType(value)
+		router.push(pathname + '?' + createQueryString('type', value))
 	}
 
 	const setDirection = (value: DirectionEnum) => {
 		setCourseDirection(value)
+		router.push(pathname + '?' + createQueryString('dir', value))
 	}
+
+	const createQueryString = useCallback(
+		(name: string, value: string) => {
+			const params = new URLSearchParams(searchParams.toString())
+			params.set(name, value)
+
+			return params.toString()
+		},
+		[searchParams]
+	)
 
 	return (
 		<div className='tw-flex tw-flex-col tw-justify-center tw-w-[600px] tw-h-screen tw-py-[40px] tw-mx-auto'>
