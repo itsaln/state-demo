7dc0777d69f6969a08b0bd8d8e771b27a5d2d932
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-05-17 11:51:32 +0500

    useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,30 @@
 import { Catalog } from '@/components/catalog/Catalog'
 import { Filter } from '@/components/filter/Filter'
 import { TypeEnum, DirectionEnum } from '@/components/filter/constants'
+import { useState } from 'react'
 
 export default function Home() {
-	const setType = (value: TypeEnum) => {}
+	const [courseType, setCourseType] = useState(TypeEnum.COURSE)
+	const [courseDirection, setCourseDirection] = useState(DirectionEnum.FRONTEND)
 
-	const setDirection = (value: DirectionEnum) => {}
+	const setType = (value: TypeEnum) => {
+		setCourseType(value)
+	}
+
+	const setDirection = (value: DirectionEnum) => {
+		setCourseDirection(value)
+	}
 
 	return (
 		<div className='tw-flex tw-flex-col tw-justify-center tw-w-[600px] tw-h-screen tw-py-[40px] tw-mx-auto'>
 			<Filter
-				courseType={TypeEnum.COURSE}
-				courseDirection={DirectionEnum.FRONTEND}
+				courseType={courseType}
+				courseDirection={courseDirection}
 				setCourseType={setType}
 				setCourseDirection={setDirection}
 			/>
 
-			<Catalog
-				courseType={TypeEnum.COURSE}
-				courseDirection={DirectionEnum.FRONTEND}
-			/>
+			<Catalog courseType={courseType} courseDirection={courseDirection} />
 		</div>
 	)
 }
