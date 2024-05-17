bfe6580ea7ea93e532e875f33d61cafb5a6fef9e
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-05-17 12:02:34 +0500

    Удаление useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useState } from 'react'
+import { useCallback } from 'react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 import { Catalog } from '@/components/catalog/Catalog'
@@ -12,20 +12,11 @@ export default function Home() {
 	const pathname = usePathname()
 	const searchParams = useSearchParams()
 
-	const [courseType, setCourseType] = useState(
-		searchParams.get('type') ?? TypeEnum.COURSE
-	)
-	const [courseDirection, setCourseDirection] = useState(
-		searchParams.get('dir') ?? DirectionEnum.FRONTEND
-	)
-
 	const setType = (value: TypeEnum) => {
-		setCourseType(value)
 		router.push(pathname + '?' + createQueryString('type', value))
 	}
 
 	const setDirection = (value: DirectionEnum) => {
-		setCourseDirection(value)
 		router.push(pathname + '?' + createQueryString('dir', value))
 	}
 
@@ -42,13 +33,16 @@ export default function Home() {
 	return (
 		<div className='tw-flex tw-flex-col tw-justify-center tw-w-[600px] tw-h-screen tw-py-[40px] tw-mx-auto'>
 			<Filter
-				courseType={courseType}
-				courseDirection={courseDirection}
+				courseType={searchParams.get('type') ?? TypeEnum.COURSE}
+				courseDirection={searchParams.get('dir') ?? DirectionEnum.FRONTEND}
 				setCourseType={setType}
 				setCourseDirection={setDirection}
 			/>
 
-			<Catalog courseType={courseType} courseDirection={courseDirection} />
+			<Catalog
+				courseType={searchParams.get('type') ?? TypeEnum.COURSE}
+				courseDirection={searchParams.get('dir') ?? DirectionEnum.FRONTEND}
+			/>
 		</div>
 	)
 }
